2e96086772431a4366d2f2cf513025856e1a57c8
Jisoo Kim <81529859+jisoo98kim@users.noreply.github.com>
2022-02-01 15:42:38 +0900

    Create BOJ (1330) -  두 수 비교하기.py

---
+++ Baekjoon Online Judge/BOJ (1330) -  두 수 비교하기.py
@@ -0,0 +1,14 @@
+# Link: https://www.acmicpc.net/source/38385387
+
+if __name__ == '__main__':
+    fir, sec = map(int, input().split(' ')) # To receive the user input 
+
+    # Compare two values
+    if fir > sec:
+        print('>')
+
+    elif fir < sec:
+        print('<')
+
+    else:
+        print('==')

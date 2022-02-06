ec10f4bfcbaa7a9ba5bc634724242a2e3df79c7f
Jisoo Kim <jisoo98kim@gmail.com>
2022-02-06 14:15:21 +0900

    BOJ (14681) - 사분면 고르기.py

---
+++ Baekjoon Online Judge/BOJ (14681) - 사분면 고르기.py
@@ -0,0 +1,14 @@
+#Link: https://www.acmicpc.net/source/39060390
+
+# Quadrant Check
+x = int(input())
+y = int(input())
+
+if x > 0 and y > 0:
+    print('1')
+elif x < 0 and y > 0:
+    print('2')
+elif x < 0 and y < 0:
+    print('3')
+else:
+    print('4')

021bb97f8166303e5a663f6cf19d5ae6c6c25f6a
Jisoo Kim <jisoo98kim@gmail.com>
2022-03-10 23:11:03 +0900

    BOJ (15596) - 정수 N개의 합.py

---
+++ Baekjoon Online Judge/BOJ (15596) - 정수 N개의 합.py
@@ -0,0 +1,14 @@
+# Link: https://www.acmicpc.net/source/40956398
+
+# Solution 1
+def solve(a):
+    return sum(a)
+
+# Solution 2
+"""
+def solve(a):
+    total = 0
+    for i in a:
+        ans += i
+    return ans
+    """

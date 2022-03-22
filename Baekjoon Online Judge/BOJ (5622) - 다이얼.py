f7d81cc4e6d06ad914a8a8d37042fd1512aa8ebb
Jisoo Kim <jisoo98kim@gmail.com>
2022-03-22 23:11:55 +0900

    BOJ (5622) - 다이얼.py

---
+++ Baekjoon Online Judge/BOJ (5622) - 다이얼.py
@@ -0,0 +1,12 @@
+# Link: https://www.acmicpc.net/source/41074288
+
+dial = input()
+
+dict = {'A': 2, 'B': 2, 'C': 2, 'D': 3, 'E': 3, 'F': 3, 'G': 4, 'H':4, 'I': 4,
+        'J': 5, 'K': 5, 'L': 5, 'M': 6, 'N': 6, 'O': 6, 'P': 7, 'Q': 7, 'R': 7, 'S': 7,
+        'T': 8, 'U': 8, 'V': 8, 'W': 9, 'X': 9, 'Y': 9, 'Z': 9}
+
+sec = 0
+for i in dial:
+    sec += dict[i] + 1
+print(sec)
